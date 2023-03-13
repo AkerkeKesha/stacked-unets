ddc1ea63e61b052f6e7cf75d89fa662dd9b73285
Akerke Balgabekova <akerke.balgabekova@sanosgroup.co>
2023-03-13 22:49:27 +0100

    created get test df in order use it for main and visualize prediction

--- src/predict.py
+++ src/predict.py
@@ -7,10 +7,13 @@ from tqdm.notebook import tqdm
 from utils import create_df
 
 
+def get_test_df():
+    return create_df(config.test_dir, split="test")
+
+
 def predict():
     final_predictions = []
-
-    test_df = create_df(config.test_dir, split="test")
+    test_df = get_test_df()
     test_dataset = ETCIDataset(test_df, split="test", transform=None)
     test_loader = DataLoader(test_dataset, batch_size=config.batch_size, shuffle=False, num_workers=config.num_workers)
 
