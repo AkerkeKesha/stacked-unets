1f809728c4f91739bd79d1e6f53e7e3d33720856
Akerke Balgabekova <akerke.balgabekova@sanosgroup.co>
2023-03-13 21:36:33 +0100

    changed config for colab

--- config.py
+++ config.py
@@ -1,10 +1,20 @@
 import os
 
-project_root = os.path.dirname(os.path.abspath(__file__))
-# dataset related
-etci_dataset = os.path.join(project_root, 'dataset', 'data-etci-flood')
-train_dir = os.path.join(etci_dataset, "train")
-test_dir = os.path.join(etci_dataset, "test_internal")
+
+is_colab = True
+
+if not is_colab:
+    project_root = os.path.dirname(os.path.abspath(__file__))
+    # dataset related
+    etci_dataset = os.path.join(project_root, 'dataset', 'data-etci-flood')
+    train_dir = os.path.join(etci_dataset, "train")
+    test_dir = os.path.join(etci_dataset, "test_internal")
+else:
+    # dataset related
+    etci_dataset = '/content/ETCI_2021_Competition_Dataset/'
+    train_dir = os.path.join(etci_dataset, "train")
+    test_dir = os.path.join(etci_dataset, "val")
+
 
 # hyperparameters
 num_workers = 8
