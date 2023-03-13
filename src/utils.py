05e10e9f68617c5271333b146c55d2748eed6cb2
Akerke Balgabekova <akerke.balgabekova@sanosgroup.co>
2023-03-13 22:50:27 +0100

    cleaned up a bit, created prediction visualization function

--- src/utils.py
+++ src/utils.py
@@ -4,9 +4,9 @@ import pathlib
 import os
 import numpy as np
 import pandas as pd
-import config
+import cv2
 import logging
-
+import matplotlib.pyplot as plt
 
 logging.basicConfig(filename="single_Unet_training.log",
                              filemode="w",
@@ -21,15 +21,6 @@ def grayscale_to_rgb(vv_image, vh_image):
     ratio_image = np.clip(np.nan_to_num(vh_image/(vv_image + 1e-6), 0), 0, 1)
     rgb_image = np.stack((vv_image, vh_image, 1-ratio_image), axis=2)
     return rgb_image
-
-
-def visualize():
-    pass
-
-
-def _validate_loader():
-    logging.info(f"Number of training temporal - regions: {len(glob(config.train_dir + '/*/'))}")
-    logging.info(f"Number of test temporal - regions: {len(glob(config.test_dir + '/*/'))}")
 
 
 def create_df(dirname, split):
@@ -63,4 +54,18 @@ def create_df(dirname, split):
         "region": region_names,
     }
     return pd.DataFrame(paths)
+
+
+def visualize_pred(df_row, prediction, figsize=[25, 15]):
+    vv_image = cv2.imread(df_row['vv_image_path'], 0) / 255.0
+    vh_image = cv2.imread(df_row['vh_image_path'], 0) / 255.0
+    rgb_input = grayscale_to_rgb(vv_image, vh_image)
+
+    plt.figure(figsize=tuple(figsize))
+    plt.subplot(1, 2, 1)
+    plt.imshow(rgb_input)
+    plt.title('RGB w/ result')
+    plt.subplot(1, 2, 2)
+    plt.imshow(prediction)
+    plt.title('Prediction')
 
