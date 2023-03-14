5b17a3d6864ce0cddec6419ef2e446b6b4b1946a
Akerke Balgabekova <akerke.balgabekova@sanosgroup.co>
2023-03-14 08:38:06 +0100

    changed val_loss and train_loss lists to store average loss per epoch

--- src/train.py
+++ src/train.py
@@ -22,19 +22,24 @@ def train(num_epochs):
     for epoch in range(num_epochs):
         print(f"Epoch: [{epoch + 1} / {num_epochs}]")
         model.train()
+        training_loss = 0
         for batch in tqdm(train_loader):
             image = batch["image"].to(device)
             mask = batch["mask"].to(device)
             pred = model(image)
             loss = criterion(pred, mask)
             optimizer.zero_grad()
+            training_loss = loss.item()
             loss.backward()
             optimizer.step()
-            train_losses.append(loss.item())
             iou_metric.update(pred.detach().cpu().numpy(), mask.cpu().numpy())
         mean_iou = iou_metric.mean_iou()
         print(f"Train mean IoU = {mean_iou:.4f}")
         iou_metric.reset()
+        # Take the average losses
+        training_loss = training_loss / len(train_loader)
+        train_losses.append(training_loss)
+
     model.eval()
     iou_metric = IntersectionOverUnion(num_classes=2)
     with torch.no_grad():
@@ -43,11 +48,14 @@ def train(num_epochs):
             mask = batch["mask"].to(device)
             pred = model(image)
             loss = criterion(pred, mask)
-            val_losses.append(loss.item())
+            val_loss = loss.item()
             iou_metric.update(pred.detach().cpu().numpy(), mask.cpu().numpy())
         mean_iou = iou_metric.mean_iou()
         print(f"Val mean IoU = {mean_iou:.4f}")
         iou_metric.reset()
+        # Take the average losses
+        val_loss = val_loss / len(val_loader)
+        val_losses.append(val_loss)
 
     torch.save(model.state_dict(), "output/single_unet.pt")
     return train_losses, val_losses
