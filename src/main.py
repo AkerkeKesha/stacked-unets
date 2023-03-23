67bb3696a3daae74ff4615df71de159f928b5cd8
Akerke Balgabekova <akerke.balgabekova@sanosgroup.co>
2023-03-23 21:41:01 +0100

    changed main function into minimal basic unet model

--- src/main.py
+++ src/main.py
@@ -1,21 +1,22 @@
 import time
 from train import train
-from predict import predict, get_test_df
+from predict import predict
 import numpy as np
 import config
-from utils import visualize_pred
+from utils import visualize_prediction, get_etci_df
 import matplotlib.pyplot as plt
 
 
-if __name__ == '__main__':
+def start_basic_unet():
     start = time.time()
     train_losses, val_losses = train(config.num_epochs)
     print(f"{time.time() - start} seconds to train")
-    np.save('train_losses.npy', train_losses)
-    np.save('val_losses.npy', val_losses)
+    np.save(f'{config.output_dir}/train_losses.npy', train_losses)
+    np.save(f'{config.output_dir}/val_losses.npy', val_losses)
     print(f"Done saving average losses")
-    train_losses = np.load('train_losses.npy')
-    val_losses = np.load('val_losses.npy')
+
+    train_losses = np.load(f'{config.output_dir}/train_losses.npy')
+    val_losses = np.load(f'{config.output_dir}/val_losses.npy')
     plt.plot(train_losses, label="Training Loss")
     plt.plot(val_losses, label="Validation Loss")
     plt.title("Loss values")
@@ -23,8 +24,12 @@ if __name__ == '__main__':
     plt.ylabel("Loss")
     plt.legend()
     plt.show()
+    plt.savefig(f'{config.output_dir}/loss_plot.png', bbox='tight')
 
     final_predictions = predict()
+    np.save(f'{config.output_dir}/predictions.npy',  fix_imports=True, allow_pickle=False)
+    # TODO: couple of random indices or all test
     index = -100
-    test_df = get_test_df()
-    visualize_pred(test_df.iloc[index], final_predictions[index], figsize=(17, 10))
+    test_df = get_etci_df(config.test_dir, split="test")
+    visualize_prediction(test_df.iloc[index], final_predictions[index], figure_size=(17, 10))
+
