2e304c5bfa457606697d58239f8bd3bd8d455f4d
Akerke Balgabekova <akerke.balgabekova@sanosgroup.co>
2023-03-23 21:40:01 +0100

    added ideas for spacenet6

--- src/dataset.py
+++ src/dataset.py
@@ -1,13 +1,14 @@
-"""
-Reference:
-https://medium.com/cloud-to-street/jumpstart-your-machine-learning-satellite-competition-submission-2443b40d0a5a
-"""
 import cv2
 from torch.utils.data import Dataset
 import utils
+from PIL import Image
 
 
 class ETCIDataset(Dataset):
+    """
+    Reference:
+    https://medium.com/cloud-to-street/jumpstart-your-machine-learning-satellite-competition-submission-2443b40d0a5a
+    """
     def __init__(self, dataframe, split, transform=None):
         self.dataset = dataframe
         self.split = split
@@ -23,6 +24,7 @@ class ETCIDataset(Dataset):
         vv_image = cv2.imread(df_row["vv_image_path"], 0) / 255.0
         vh_image = cv2.imread(df_row["vh_image_path"], 0) / 255.0
 
+        # TODO: remove from training
         rgb_image = utils.grayscale_to_rgb(vv_image, vh_image)
 
         if self.split == "test":
@@ -37,6 +39,45 @@ class ETCIDataset(Dataset):
             example["mask"] = flood_mask.astype('int64')
 
         return example
+
+
+class SN6Dataset(Dataset):
+    def __init__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __getitem__(self, item):
+        pass
+
+
+class Spacenet6Dataset(Dataset):
+    def __init__(self, data_dir, transforms=None):
+        self.data_dir = data_dir
+        self.transforms = transforms
+
+        # get list of image and mask files
+        self.image_files = sorted(os.listdir(os.path.join(self.data_dir, "images")))
+        self.mask_files = sorted(os.listdir(os.path.join(self.data_dir, "masks")))
+
+    def __getitem__(self, index):
+        # load image and mask
+        image_path = os.path.join(self.data_dir, "images", self.image_files[index])
+        mask_path = os.path.join(self.data_dir, "masks", self.mask_files[index])
+        image = np.array(Image.open(image_path).convert("RGB"))
+        mask = np.array(Image.open(mask_path).convert("L"))
+
+        # apply transforms
+        if self.transforms:
+            transformed = self.transforms(image=image, mask=mask)
+            image = transformed["image"]
+            mask = transformed["mask"]
+
+        return image, mask
+
+    def __len__(self):
+        return len(self.image_files)
 
 
 
