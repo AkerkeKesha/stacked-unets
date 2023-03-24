5f544fb0d1012052353b415a89ba935bd4a68676
Akerke Balgabekova <akerke.balgabekova@sanosgroup.co>
2023-03-24 14:36:13 +0100

    cleaned predict dataframe from noise

--- src/predict.py
+++ src/predict.py
@@ -4,12 +4,14 @@ from dataset import ETCIDataset
 from model import create_single_unet
 import config
 from tqdm.notebook import tqdm
-from utils import get_etci_df
+from utils import get_etci_df, cleanup_etci_data
 
 
 def predict():
     final_predictions = []
     test_df = get_etci_df(config.test_dir, split="test")
+    test_df = cleanup_etci_data(test_df)
+
     test_dataset = ETCIDataset(test_df, split="test", transform=None)
     test_loader = DataLoader(test_dataset, batch_size=config.batch_size, shuffle=False, num_workers=config.num_workers)
 
@@ -24,6 +26,7 @@ def predict():
         for batch in tqdm(test_loader):
             image = batch["image"].to(device)
             pred = model(image)
+
             class_label = pred.argmax(dim=1)
             class_label = class_label.detach().cpu().numpy()
             final_predictions.append(class_label)
