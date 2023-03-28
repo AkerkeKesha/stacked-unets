5c0ad61a580249158ad100989bab3ae6703c3231
Akerke Balgabekova <akerke.balgabekova@sanosgroup.co>
2023-03-28 22:03:44 +0200

    beautified code

--- src/model.py
+++ src/model.py
@@ -3,9 +3,9 @@ import segmentation_models_pytorch as smp
 
 def create_single_unet():
     model = smp.Unet(
-        encoder_name="resnet18", # TODO: read about skipconnections usage
+        encoder_name="resnet18",  # TODO: read about skipconnections usage
         encoder_weights=None,
-        decoder_use_batchnorm=False, # TODO: 2 images, do not train on RGB image
+        decoder_use_batchnorm=False,  # TODO: 2 images, do not train on RGB image
         in_channels=3,  # TODO: 2 images, do not train on RGB image
         classes=2,  # TODO water(contains flood) and land as labels
     )
