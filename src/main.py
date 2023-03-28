ed62cb392a9c0021b1b12ffb38ec754de96e9de8
Akerke Balgabekova <akerke.balgabekova@sanosgroup.co>
2023-03-28 20:00:21 +0200

    refactor: test_df passed to predict function

--- src/main.py
+++ src/main.py
@@ -2,7 +2,6 @@ import time
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-import cv2
 from train import train
 from predict import predict
 import config
@@ -36,11 +35,10 @@ def start_basic_unet():
     plt.savefig(f'{config.output_dir}/loss_plot.png', bbox='tight')
     plt.show()
 
-    final_predictions = predict()
-    np.save(f'{config.output_dir}/predictions.npy', final_predictions, fix_imports=True, allow_pickle=False)
     test_df = get_etci_df(config.test_dir, split="test")
     test_df = cleanup_etci_data(test_df)
-
+    final_predictions = predict(test_df)
+    np.save(f'{config.output_dir}/predictions.npy', final_predictions, fix_imports=True, allow_pickle=False)
     output_dir = f"{config.target_dir}"
     plot_all_predictions(test_df, final_predictions, output_dir)
     print(f"Prediction plots saved")
