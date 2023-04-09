9358abc04b5753d3dd1baac388e56f6e171acbe5
Akerke Balgabekova <akerke.balgabekova@sanosgroup.co>
2023-04-09 20:49:33 +0200

    fix: choose correct var to store outputs

--- src/main.py
+++ src/main.py
@@ -48,5 +48,5 @@ def start_basic_unet():
 
     for index in range(len(final_predictions)):
         visualize_prediction(test_df.iloc[index], final_predictions[index],
-                             output_dir=f'{config.output_dir}/{config.dataset_name}_labels', figure_size=(17, 10))
+                             output_dir=f'{config.output_dir}/{config.dataset}_labels', figure_size=(17, 10))
     print(f"All predictions finished plotting")
