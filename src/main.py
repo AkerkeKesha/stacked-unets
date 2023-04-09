a56eec52558511d0e925145a373217c9a54a7030
Akerke Balgabekova <akerke.balgabekova@sanosgroup.co>
2023-04-09 10:42:00 +0200

    added iouc arrays, their plots and all predictions plot

--- src/main.py
+++ src/main.py
@@ -10,11 +10,14 @@ from utils import visualize_prediction, get_etci_df, cleanup_etci_data
 
 def start_basic_unet():
     start = time.time()
-    train_losses, val_losses = train(config.num_epochs)
+    train_losses, val_losses, train_iou, val_iou = train(config.num_epochs)
     print(f"{time.time() - start} seconds to train")
     np.save(f'{config.output_dir}/train_losses.npy', train_losses)
     np.save(f'{config.output_dir}/val_losses.npy', val_losses)
     print(f"Done saving average losses")
+    np.save(f'{config.output_dir}/train_iou.npy', train_iou)
+    np.save(f'{config.output_dir}/val_iou.npy', val_iou)
+    print(f"Done saving evaluation metrics")
 
     train_losses = np.load(f'{config.output_dir}/train_losses.npy')
     val_losses = np.load(f'{config.output_dir}/val_losses.npy')
@@ -27,14 +30,24 @@ def start_basic_unet():
     plt.savefig(f'{config.output_dir}/loss_plot.png', bbox='tight')
     plt.show()
 
+    train_iou = np.load(f'{config.output_dir}/train_iou.npy')
+    val_iou = np.load(f'{config.output_dir}/val_iou.npy')
+    epochs = range(1, len(train_iou) + 1)
+    plt.plot(epochs, train_iou, label="Training Mean IoU")
+    plt.plot(epochs, val_iou, label="Validation Mean IoU")
+    plt.xlabel("Epoch")
+    plt.ylabel("Mean IoU")
+    plt.legend()
+    plt.title("Mean IoU over Epochs")
+    plt.savefig(f'{config.output_dir}/iou_plot.png', bbox='tight')
+    plt.show()
+
     test_df = get_etci_df(config.test_dir, split="test")
     test_df = cleanup_etci_data(test_df)
     final_predictions = predict(test_df)
     np.save(f'{config.output_dir}/predictions.npy', final_predictions, fix_imports=True, allow_pickle=False)
 
-    for index in range(10):
-        visualize_prediction(test_df.iloc[index], final_predictions[index], figure_size=(17, 10))
-    print(f"First 10 predictions plotted")
-    for index in range(-10, 0, 1):
-        visualize_prediction(test_df.iloc[index], final_predictions[index], figure_size=(17, 10))
-    print(f"Last 10 predictions plotted")
+    for index in range(len(final_predictions)):
+        visualize_prediction(test_df.iloc[index], final_predictions[index],
+                             output_dir=f'{config.output_dir}/{config.dataset_name}_labels', figure_size=(17, 10))
+    print(f"All predictions finished plotting")
