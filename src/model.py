52538d87c06652d18829b6b8842d18b8ffd9f9fd
Akerke Balgabekova <akerke.balgabekova@sanosgroup.co>
2023-04-18 21:37:15 +0200

    changed input channels wrt to dataset

--- src/model.py
+++ src/model.py
@@ -1,13 +1,18 @@
 import segmentation_models_pytorch as smp
 
+import config
+
 
 def create_single_unet():
+    if config.dataset == "sn6":
+        input_channels = 4
+    else:
+        input_channels = 2
     model = smp.Unet(
         encoder_name="resnet18",  # TODO: read about skipconnections usage
         encoder_weights=None,
         decoder_use_batchnorm=False,
-        # in_channels=1,  # TODO: grayscale 1 channel, RGB 3 channels
-        in_channels=2,
-        classes=2,  # TODO water(contains flood) and land as labels
+        in_channels=input_channels,
+        classes=2,
     )
     return model
