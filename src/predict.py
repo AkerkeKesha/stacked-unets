8fc44c9f11826fcaad6877294ef52df66e9ccc62
Akerke Balgabekova <akerke.balgabekova@sanosgroup.co>
2023-05-04 15:38:34 +0200

    added evaluation metrics on test set

--- src/predict.py
+++ src/predict.py
@@ -1,35 +1,37 @@
 import torch
 import numpy as np
-from torch.utils.data import DataLoader
 from tqdm.notebook import tqdm
-from dataset import ETCIDataset, SN6Dataset
 from model import create_single_unet
+from evaluate import IntersectionOverUnion
 import config
 
 
-def predict(test_df, dataset_name):
-    final_predictions = []
-    if dataset_name == "sn6":
-        test_dataset = SN6Dataset(dataframe=test_df, split="test", transform=None)
-    else:
-        test_dataset = ETCIDataset(test_df, split="test", transform=None)
-    test_loader = DataLoader(test_dataset, batch_size=config.batch_size, shuffle=False, num_workers=config.num_workers)
-
+def predict(test_loader):
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     model = create_single_unet()
     model.load_state_dict(torch.load(f"{config.output_dir}/single_unet_{config.dataset}.pt"))
     model.to(device)
     model.eval()
+
+    final_predictions, true_labels = [], []
+    iou_metric = IntersectionOverUnion(num_classes=2)
     try:
         with torch.no_grad():
             for batch in tqdm(test_loader):
                 image = batch["image"].to(device)
+                true_mask = batch["mask"].numpy()  # Store true masks
+                true_labels.append(true_mask)
+
                 pred = model(image)
                 class_label = pred.argmax(dim=1)
                 class_label = class_label.detach().cpu().numpy()
                 final_predictions.append(class_label.astype("uint8"))
+                iou_metric.update(class_label, true_mask)
+
     except Exception as te:
         print(f"An exception occurred during inference: {te}")
 
+    mean_iou = iou_metric.mean_iou()
+    print(f"Mean IoU for the test dataset: {mean_iou}")
     final_predictions = np.concatenate(final_predictions, axis=0)  # a single array of prediction from all batches
     return final_predictions
