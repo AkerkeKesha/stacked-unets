7134dfde9a663ce5941a7294624ea927a5112224
Akerke Balgabekova <akerke.balgabekova@sanosgroup.co>
2023-05-04 15:35:25 +0200

    removed redundant test_dir

--- config.py
+++ config.py
@@ -1,26 +1,37 @@
 import os
-import argparse
 
+environment = ""
+try:
+    from google.colab import drive
+    environment = "colab"
+except ImportError:
+    pass
 
-# is_colab = False
-is_colab = True
+try:
+    import kaggle
+    environment = "kaggle"
+except ImportError:
+    pass
 
-if not is_colab:
+if environment == "":
+    environment = "local"
+
+if environment == "local":
     project_root = os.path.dirname(os.path.abspath(__file__))
+elif environment == "colab":
+    project_root = "/content/stacked-unets"
 else:
-    project_root = '/content/stacked-unets'
-output_dir = os.path.join(project_root, 'output')
+    project_root = "/kaggle"
 
+output_dir = os.path.join(project_root, 'output')
 # dataset = "etci"
 dataset = "sn6"
 if dataset == 'etci':
     dataset_name = 'data-etci-flood'
     train_dir = os.path.join(project_root, 'dataset', dataset_name, 'train')
-    test_dir = os.path.join(project_root, 'dataset', dataset_name, 'test_internal')
 elif dataset == 'sn6':
     dataset_name = 'data-spacenet6'
     train_dir = os.path.join(project_root, 'dataset', dataset_name, 'train', 'AOI_11_Rotterdam')
-    test_dir = os.path.join(project_root, 'dataset', dataset_name, 'test_public', 'AOI_11_Rotterdam')
     mask_train_dir = os.path.join(train_dir, 'masks')
     sn6_summary_datapath = os.path.join(train_dir, 'SummaryData', 'SN6_Train_AOI_11_Rotterdam_Buildings.csv')
 
@@ -30,5 +41,5 @@ batch_size = 48
 
 # training related
 learning_rate = 1e-3
-num_epochs = 5
+num_epochs = 20
 
