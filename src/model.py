9ed0adfa070c866beff2643ed8596426ffcee225
Akerke Balgabekova <akerke.balgabekova@sanosgroup.co>
2023-06-06 15:43:24 +0200

    created stacked unet class that creates level0, level1 models using base model

--- src/model.py
+++ src/model.py
@@ -1,18 +1,34 @@
 import segmentation_models_pytorch as smp
-
 import config
+import torch.nn as nn
+from copy import deepcopy
 
 
-def create_single_unet():
-    if config.dataset == "sn6":
-        input_channels = 4
-    else:
-        input_channels = 2
-    model = smp.Unet(
-        encoder_name="resnet18",  # TODO: read about skipconnections usage
+class StackedUNet(nn.Module):
+    def __init__(self, n_levels, base_model):
+        super().__init__()
+        self.n_levels = n_levels
+        self.base_model = base_model
+        if n_levels > 0:
+            self.models = nn.ModuleList([deepcopy(base_model) for _ in range(n_levels)])
+        else:
+            self.models = None
+
+    def forward(self, x):
+        if self.n_levels > 0:
+            out = x
+            for i in range(self.n_levels):
+                out = self.models[i](out)
+            return out
+        else:
+            return self.base_model(x)
+
+
+basic_unet = smp.Unet(
+        encoder_name="resnet18",
         encoder_weights=None,
         decoder_use_batchnorm=False,
-        in_channels=input_channels,
+        in_channels=4 if config.dataset == "sn6" else 2,
         classes=2,
-    )
-    return model
+)
+
