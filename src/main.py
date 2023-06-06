4c0b7408a8b3ab0ec8f6247be9364486bd6ac6cb
Akerke Balgabekova <akerke.balgabekova@sanosgroup.co>
2023-06-06 17:19:15 +0200

    put dataset split outside of loader, modified prediction plot that reuses saved semantic map, added part that draws couple of image results

--- src/main.py
+++ src/main.py
@@ -2,18 +2,25 @@ import time
 import math
 import os
 import numpy as np
+from glob import glob
 import matplotlib.pyplot as plt
 import config
 from train import train
 from predict import predict
-from dataloader import get_loader
-from utils import plot_single_prediction
+from dataloader import get_loader, split_etci_data, split_sn6_data
+from utils import plot_single_prediction, visualize_prediction
 
 
 def start_basic_unet():
     start = time.time()
-    train_loader, val_loader, test_loader = get_loader(config.dataset)
-    train_losses, val_losses, train_iou, val_iou = train(config.num_epochs, train_loader, val_loader)
+    if config.dataset == "etci":
+        original_df, train_df, val_df, test_df = split_etci_data()
+        train_loader, val_loader, test_loader = get_loader("etci", train_df, val_df, test_df)
+    else:
+        original_df, train_df, val_df, test_df = split_sn6_data()
+        train_loader, val_loader, test_loader = get_loader("sn6", train_df, val_df, test_df)
+
+    train_losses, val_losses, train_iou, val_iou = train(config.num_epochs, train_loader, val_loader, train_df, val_df)
     print(f"{time.time() - start} seconds to train")
     np.save(f'{config.output_dir}/train_losses_{config.dataset}.npy', train_losses)
     np.save(f'{config.output_dir}/val_losses_{config.dataset}.npy', val_losses)
@@ -45,19 +52,29 @@ def start_basic_unet():
     plt.savefig(f'{config.output_dir}/iou_plot_{config.dataset}.png', bbox_inches='tight')
     plt.show()
 
-    final_predictions = predict(test_loader)
+    final_predictions = predict(test_loader, test_df)
     np.save(f'{config.output_dir}/predictions_{config.dataset}.npy', final_predictions, fix_imports=True, allow_pickle=False)
-    n_batches = math.ceil(len(final_predictions) / config.batch_size)
+    n_batches = math.ceil(len(test_df) / config.batch_size)
     for batch in range(n_batches):
         start = batch * config.batch_size
-        end = min((batch + 1) * config.batch_size, len(final_predictions))
+        end = min((batch + 1) * config.batch_size, len(test_df))
         for index in range(start, end):
-            original_index = index
-            df_row = test_loader.dataset.dataset.iloc[original_index]
+            df_row = test_df.iloc[index]
             vv_image_path = df_row["vv_image_path"]
             image_id = os.path.basename(vv_image_path).split('.')[0]
-            plot_single_prediction(image_id, final_predictions[index],
-                                   f"{config.output_dir}/{config.dataset_name}_labels)")
-
+            semantic_map_path = df_row["semantic_map_prev_level"]
+            plot_single_prediction(image_id, semantic_map_path, f"{config.output_dir}/{config.dataset_name}_labels")
         print(f"Finished plotting batch {batch + 1}/{n_batches}")
     print(f"All predictions finished plotting")
+
+    labels_dir = os.path.join(config.output_dir, 'etci_labels')
+    image_ids = []
+    for file_path in glob(f'{labels_dir}/*.png'):
+        filename = os.path.basename(file_path)
+        parts = filename.split('_')
+        image_id = '_'.join(parts[1:]).split('.')[0]
+        image_ids.append(image_id)
+
+    for index in config.SAMPLE_INDICES:
+        visualize_prediction(image_ids[index], original_df, labels_dir)
+    print(f"Finished visualizing some predictions.")
