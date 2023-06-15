2bbf59ed30641e477da763872a29957a2040b569
Akerke Balgabekova <akerke.balgabekova@sanosgroup.co>
2023-06-15 14:34:46 +0200

    beautified code

--- config.py
+++ config.py
@@ -12,14 +12,14 @@ else:
 output_dir = os.path.join(project_root, 'output')
 dataset = "etci"
 # dataset = "sn6"
-if dataset == 'etci':
-    dataset_name = 'data-etci-flood'
-    train_dir = os.path.join(project_root, 'dataset', dataset_name, 'train')
-elif dataset == 'sn6':
-    dataset_name = 'data-spacenet6'
-    train_dir = os.path.join(project_root, 'dataset', dataset_name, 'train', 'AOI_11_Rotterdam')
+if dataset == "etci":
+    dataset_name = "data-etci-flood"
+    train_dir = os.path.join(project_root, "dataset", dataset_name, "train")
+elif dataset == "sn6":
+    dataset_name = "data-spacenet6"
+    train_dir = os.path.join(project_root, "dataset", dataset_name, "train", "AOI_11_Rotterdam")
     mask_train_dir = os.path.join(train_dir, 'masks')
-    sn6_summary_datapath = os.path.join(train_dir, 'SummaryData', 'SN6_Train_AOI_11_Rotterdam_Buildings.csv')
+    sn6_summary_datapath = os.path.join(train_dir, "SummaryData", "SN6_Train_AOI_11_Rotterdam_Buildings.csv")
 
 # hyperparameters
 num_workers = 1 if environment == "local" else 2
@@ -30,7 +30,7 @@ learning_rate = 1e-3
 num_epochs = 2 if environment == "local" else 20
 
 
-if dataset == 'etci':
+if dataset == "etci":
     SAMPLE_INDICES = [
         2078,
         2047,
