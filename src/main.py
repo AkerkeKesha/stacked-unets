6efe8d12eeac627087a23be43f1b8e856a2699de
Akerke Balgabekova <akerke.balgabekova@sanosgroup.co>
2023-06-22 20:12:40 +0200

    refactorede basic unet with level 0

--- src/main.py
+++ src/main.py
@@ -11,49 +11,40 @@ from dataloader import get_loader, split_etci_data, split_sn6_data
 from utils import plot_single_prediction, visualize_prediction, get_image_name_from_path
 
 
-def start_basic_unet():
-    start = time.time()
-    if config.dataset == "etci":
+def load_data(dataset):
+    if dataset == "etci":
         original_df, train_df, val_df, test_df = split_etci_data()
         train_loader, val_loader, test_loader = get_loader("etci", train_df, val_df, test_df)
     else:
         original_df, train_df, val_df, test_df = split_sn6_data()
         train_loader, val_loader, test_loader = get_loader("sn6", train_df, val_df, test_df)
+    return original_df, train_df, val_df, test_df, train_loader, val_loader, test_loader
 
-    train_losses, val_losses, train_iou, val_iou = train(config.num_epochs, train_loader, val_loader, train_df, val_df)
-    print(f"{time.time() - start} seconds to train")
+
+def save_metrics(train_iou, train_losses, val_iou, val_losses):
     np.save(f'{config.output_dir}/train_losses_{config.dataset}.npy', train_losses)
     np.save(f'{config.output_dir}/val_losses_{config.dataset}.npy', val_losses)
-    print(f"Done saving average losses")
     np.save(f'{config.output_dir}/train_iou_{config.dataset}.npy', train_iou)
     np.save(f'{config.output_dir}/val_iou_{config.dataset}.npy', val_iou)
-    print(f"Done saving evaluation metrics on train/val")
+    print(f"Done saving evaluation metrics/losses on train/val")
 
-    train_losses = np.load(f'{config.output_dir}/train_losses_{config.dataset}.npy')
-    val_losses = np.load(f'{config.output_dir}/val_losses_{config.dataset}.npy')
-    plt.plot(train_losses, label="Training Loss")
-    plt.plot(val_losses, label="Validation Loss")
-    plt.title("Loss values")
+
+def plot_metrics(metric_names, metric_labels, plot_filename, num_epochs):
+    assert len(metric_names) == len(metric_labels), "Mismatch in number of metrics and labels."
+    epochs = range(1, num_epochs + 1)
+
+    for metric_name, metric_label in zip(metric_names, metric_labels):
+        metric_values = np.load(f'{config.output_dir}/{metric_name}_{config.dataset}.npy')
+        plt.plot(epochs, metric_values, label=metric_label)
+
     plt.xlabel("Epoch")
-    plt.ylabel("Loss")
     plt.legend()
-    plt.savefig(f'{config.output_dir}/loss_plot_{config.dataset}.png', bbox_inches='tight')
+
+    plt.savefig(f'{config.output_dir}/{plot_filename}_{config.dataset}.png', bbox_inches='tight')
     plt.show()
 
-    train_iou = np.load(f'{config.output_dir}/train_iou_{config.dataset}.npy')
-    val_iou = np.load(f'{config.output_dir}/val_iou_{config.dataset}.npy')
-    epochs = range(1, len(train_iou) + 1)
-    plt.plot(epochs, train_iou, label="Training Mean IoU")
-    plt.plot(epochs, val_iou, label="Validation Mean IoU")
-    plt.xlabel("Epoch")
-    plt.ylabel("Mean IoU")
-    plt.legend()
-    plt.title("Mean IoU over Epochs")
-    plt.savefig(f'{config.output_dir}/iou_plot_{config.dataset}.png', bbox_inches='tight')
-    plt.show()
 
-    final_predictions = predict(test_loader, test_df)
-    np.save(f'{config.output_dir}/predictions_{config.dataset}.npy', final_predictions, fix_imports=True, allow_pickle=False)
+def plot_predictions(test_df):
     n_batches = math.ceil(len(test_df) / config.batch_size)
     for batch in range(n_batches):
         start = batch * config.batch_size
@@ -67,11 +58,43 @@ def start_basic_unet():
         print(f"Finished plotting batch {batch + 1}/{n_batches}")
     print(f"All predictions finished plotting")
 
+
+def visualize_results(original_df):
     labels_dir = os.path.join(config.output_dir, f'{config.dataset}_labels')
     image_ids = []
     for file_path in glob(f'{labels_dir}/*.png'):
-        image_name = get_image_name_from_path(file_path)
-        image_ids.append(image_name)
+        filename = os.path.basename(file_path)
+        parts = filename.split('_')
+        # five elements to be removed from name 'semantic_map_level_0_image_'
+        image_id = '_'.join(parts[5:]).split('.')[0]
+        image_ids.append(image_id)
     for index in config.SAMPLE_INDICES:
         visualize_prediction(image_ids[index], original_df, labels_dir)
+
+
+def start_basic_unet():
+    original_df, train_df, val_df, test_df, train_loader, val_loader, test_loader = load_data(config.dataset)
+    start = time.time()
+    train_losses, val_losses, train_iou, val_iou = train(config.num_epochs, train_loader, val_loader, train_df, val_df)
+    print(f"{time.time() - start} seconds to train")
+
+    save_metrics(train_iou, train_losses, val_iou, val_losses)
+    plot_metrics(['train_losses', 'val_losses'], ['Training Loss', 'Validation Loss'], 'loss_plot', config.num_epochs)
+    plot_metrics(['train_iou', 'val_iou'], ['Training Mean IoU', 'Validation Mean IoU'], 'iou_plot', config.num_epochs)
+
+    final_predictions = predict(test_loader, test_df)
+    np.save(f'{config.output_dir}/predictions_{config.dataset}.npy', final_predictions, fix_imports=True, allow_pickle=False)
+
+    visualize_results(original_df)
     print(f"Finished visualizing some predictions.")
+
+
+
+
+
+
+
+
+
+
+
