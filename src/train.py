635f9d301f7e5c160a9d6b81a007f5410b7d4076
Akerke Balgabekova <akerke.balgabekova@sanosgroup.co>
2023-06-26 20:57:40 +0200

    changed: - model simplified - n_levels parameter renamed for clarity - removed levels iteration - indented accordingly

--- src/train.py
+++ src/train.py
@@ -2,14 +2,14 @@ import torch.optim
 import torch.nn as nn
 import config
 from tqdm.notebook import tqdm
-from model import StackedUNet
+from model import UNet
 from utils import store_semantic_maps
 from evaluate import IntersectionOverUnion
 
 
-def train(num_epochs, train_loader, val_loader, df_train, df_val,  n_levels=0):
+def train(num_epochs, train_loader, val_loader, df_train, df_val, level=0):
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    model = StackedUNet(n_levels=n_levels)
+    model = UNet()
     model.to(device)
 
     optimizer = torch.optim.Adam(model.parameters(), lr=config.learning_rate)
@@ -17,65 +17,64 @@ def train(num_epochs, train_loader, val_loader, df_train, df_val,  n_levels=0):
 
     train_losses, val_losses = [], []
     train_ious, val_ious = [], []
-    for level in range(n_levels + 1):
-        print(f"Level: [{level + 1} / {n_levels+1}]")
-        for epoch in range(num_epochs):
-            model.train()
-            training_loss = 0
-            iou_metric = IntersectionOverUnion(num_classes=2)
-            try:
-                for batch in tqdm(train_loader):
+
+    for epoch in range(num_epochs):
+        model.train()
+        training_loss = 0
+        iou_metric = IntersectionOverUnion(num_classes=2)
+        try:
+            for batch in tqdm(train_loader):
+                image = batch["image"].to(device)
+                mask = batch["mask"].to(device)
+                pred = model(image)
+                loss = criterion(pred, mask)
+                optimizer.zero_grad()
+                training_loss += loss.item()
+                loss.backward()
+                optimizer.step()
+                iou_metric.update(pred.detach().cpu().numpy(), mask.cpu().numpy())
+        except Exception as e:
+            print(f"An exception occurred during training: {e}")
+            continue
+
+        mean_iou = iou_metric.mean_iou()
+
+        train_ious.append(mean_iou)
+        iou_metric.reset()
+        training_loss = training_loss / len(train_loader)
+        train_losses.append(training_loss)
+
+        if (epoch + 1) % 5 == 0:
+            print(f"Epoch: [{epoch + 1} / {num_epochs}]")
+            print(f"Train mean IoU = {mean_iou:.4f}")
+            print(f"Train mean loss = {training_loss:.4f}")
+
+        model.eval()
+        iou_metric = IntersectionOverUnion(num_classes=2)
+        val_loss = 0
+        try:
+            with torch.no_grad():
+                for batch in tqdm(val_loader):
                     image = batch["image"].to(device)
                     mask = batch["mask"].to(device)
                     pred = model(image)
                     loss = criterion(pred, mask)
-                    optimizer.zero_grad()
-                    training_loss += loss.item()
-                    loss.backward()
-                    optimizer.step()
+                    val_loss += loss.item()
                     iou_metric.update(pred.detach().cpu().numpy(), mask.cpu().numpy())
-            except Exception as e:
-                print(f"An exception occurred during training: {e}")
-                continue
 
-            mean_iou = iou_metric.mean_iou()
+                mean_iou = iou_metric.mean_iou()
+                val_ious.append(mean_iou)
+                iou_metric.reset()
+                val_loss = val_loss / len(val_loader)
+                val_losses.append(val_loss)
 
-            train_ious.append(mean_iou)
-            iou_metric.reset()
-            training_loss = training_loss / len(train_loader)
-            train_losses.append(training_loss)
+                if (epoch + 1) % 5 == 0:
+                    print(f"Val mean IoU = {mean_iou:.4f}")
+                    print(f"Val mean loss = {val_loss:.4f}")
 
-            if (epoch + 1) % 5 == 0:
-                print(f"Epoch: [{epoch + 1} / {num_epochs}]")
-                print(f"Train mean IoU = {mean_iou:.4f}")
-                print(f"Train mean loss = {training_loss:.4f}")
-
-            model.eval()
-            iou_metric = IntersectionOverUnion(num_classes=2)
-            val_loss = 0
-            try:
-                with torch.no_grad():
-                    for batch in tqdm(val_loader):
-                        image = batch["image"].to(device)
-                        mask = batch["mask"].to(device)
-                        pred = model(image)
-                        loss = criterion(pred, mask)
-                        val_loss += loss.item()
-                        iou_metric.update(pred.detach().cpu().numpy(), mask.cpu().numpy())
-
-                    mean_iou = iou_metric.mean_iou()
-                    val_ious.append(mean_iou)
-                    iou_metric.reset()
-                    val_loss = val_loss / len(val_loader)
-                    val_losses.append(val_loss)
-
-                    if (epoch + 1) % 5 == 0:
-                        print(f"Val mean IoU = {mean_iou:.4f}")
-                        print(f"Val mean loss = {val_loss:.4f}")
-
-            except Exception as ve:
-                print(f"An exception occurred during validation: {ve}")
-                continue
+        except Exception as ve:
+            print(f"An exception occurred during validation: {ve}")
+            continue
         for split, df, loader in [("train", df_train, train_loader), ("val", df_val, val_loader)]:
             model.eval()
             print(f"Generating semantic maps for {split} dataset...")
@@ -89,11 +88,11 @@ def train(num_epochs, train_loader, val_loader, df_train, df_val,  n_levels=0):
                         semantic_maps.append(semantic_map)
 
                 if split == "train":
-                    df_train = store_semantic_maps(df, n_levels, semantic_maps)
+                    df_train = store_semantic_maps(df, level, semantic_maps)
                 elif split == "val":
-                    df_val = store_semantic_maps(df, n_levels, semantic_maps)
+                    df_val = store_semantic_maps(df, level, semantic_maps)
 
-    torch.save(model.state_dict(), f"{config.output_dir}/level_{n_levels}_unet_{config.dataset}.pt")
+    torch.save(model.state_dict(), f"{config.output_dir}/level_{level}_unet_{config.dataset}.pt")
     return train_losses, val_losses, train_ious, val_ious, df_train, df_val
 
 
