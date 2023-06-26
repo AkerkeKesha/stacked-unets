cfcdb515ce5646681c6f710088a9feb2e6049fbd
Akerke Balgabekova <akerke.balgabekova@sanosgroup.co>
2023-06-26 20:33:08 +0200

    rolled back to simple, standard unet

--- src/model.py
+++ src/model.py
@@ -1,33 +1,22 @@
 import segmentation_models_pytorch as smp
-import config
 import torch.nn as nn
-from copy import deepcopy
 
 
-class StackedUNet(nn.Module):
-    def __init__(self, n_levels):
+class UNet(nn.Module):
+    def __init__(self):
         super().__init__()
-        self.n_levels = n_levels
 
-        self.base_model = smp.Unet(
+        self.model = smp.Unet(
             encoder_name="resnet18",
             encoder_weights=None,
-            decoder_use_batchnorm=False,
-            in_channels=3 if config.dataset == "etci" else 4,
-            classes=2,
+            in_channels=3,            # model input channels: vv, vh and prev_model_output
+            classes=2,                # binary segmentation
         )
 
-        if n_levels > 0:
-            self.models = nn.ModuleList([deepcopy(self.base_model) for _ in range(n_levels)])
-        else:
-            self.models = None
-
     def forward(self, x):
-        if self.n_levels > 0:
-            out = x
-            for i in range(self.n_levels):
-                out = self.models[i](out)
-            return out
-        else:
-            return self.base_model(x)
+        return self.model(x)
+
+
+
+
 
