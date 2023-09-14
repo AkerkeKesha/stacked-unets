2cfbae5f1a52222b43fc669a2f1cdb137820096a
Akerke Balgabekova <akerke.balgabekova@sanosgroup.co>
2023-09-14 16:35:36 +0200

    removed logging logic, print is enough

--- src/utils.py
+++ src/utils.py
@@ -9,16 +9,6 @@ import config
 import matplotlib.pyplot as plt
 
 
-# logging.basicConfig(filename="single_Unet_training.log",
-#                              filemode="w",
-#                              format="%(name)s - %(levelname)s - %(message)s",)
-#
-#
-# def get_logging():
-#     # TODO: make sure logger works
-#     return logging.getLogger()
-
-
 def grayscale_to_rgb(vv_image, vh_image):
     ratio_image = np.clip(np.nan_to_num(vh_image/(vv_image + 1e-6), 0), 0, 1)
     rgb_image = np.stack((vv_image, vh_image, 1-ratio_image), axis=2)
