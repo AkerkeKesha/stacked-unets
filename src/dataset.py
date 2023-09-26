fix: spacenet normalization, padding corrected

src/dataset.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import rasterio
 from torch.utils.data import Dataset
+from torch.nn.functional import pad
 
 
 class ETCIDataset(Dataset):
@@ -54,30 +55,34 @@ class SN6Dataset(Dataset):
         mask_path = df_row["mask_path"]
 
         with rasterio.open(image_path) as src:
-            channels = src.read()
-        max_value = channels.max()
-        channels /= max_value
-        sar_image = np.transpose(channels, (1, 2, 0))
+            sar_image = src.read()
+        sar_image /= 255.0
+        sar_image = np.transpose(sar_image, (1, 2, 0))
+
+        pad_height = 32 - (sar_image.shape[1] % 32)
+        pad_width = 32 - (sar_image.shape[2] % 32)
 
         semantic_map_path = df_row["semantic_map_prev_level"]
         if semantic_map_path:
-            semantic_map = cv2.imread(semantic_map_path, 0)
+            semantic_map = cv2.imread(semantic_map_path, 0) / 255.0
             input_image = np.dstack((sar_image, semantic_map))
         else:
             dummy_channel = np.zeros_like(sar_image[:, :, 0])
             input_image = np.dstack((sar_image, dummy_channel))
 
-        if self.split == "test":
-            example["image"] = np.transpose(input_image, (2, 0, 1)).astype('float32')
-        else:
-            mask = cv2.imread(mask_path, 0)
-            if self.transforms:
-                transformed = self.transforms(image=input_image, mask=mask)
-                input_image = transformed["image"]
-                mask = transformed["mask"]
+        mask = cv2.imread(mask_path, 0) / 255.0
 
-            example["image"] = np.transpose(input_image, (2, 0, 1)).astype('float32')
-            example["mask"] = mask.astype('int64')
+        if self.transforms:
+            transformed = self.transforms(image=input_image, mask=mask)
+            input_image = transformed["image"]
+            mask = transformed["mask"]
+
+        # Apply padding to the stacked input and mask
+        input_image = pad(input_image, (0, pad_width, 0, pad_height), mode='constant', value=0)
+        mask = pad(mask, (0, pad_width, 0, pad_height), mode='constant', value=0)
+
+        example["image"] = np.transpose(input_image, (2, 0, 1)).astype('float32')
+        example["mask"] = mask.astype('int64')
         return example
 
 