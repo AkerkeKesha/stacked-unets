8a63cb2cc26e4db63ed4ec676621e75ae3a0293c
Akerke Balgabekova <akerke.kass@gmail.com>
2023-10-04 10:22:44 +0200

    returns entropy as return vals. added mean/std calculations for input features

--- src/predict.py
+++ src/predict.py
@@ -15,21 +15,28 @@ def predict(test_loader, df_test, level=0):
     model.to(device)
     model.eval()
 
+    sum_mean = torch.zeros([3], dtype=torch.float32).to(device)
+    sum_std = torch.zeros([3], dtype=torch.float32).to(device)
+
     final_predictions, true_labels = [], []
     iou_metric = IntersectionOverUnion(num_classes=2)
-    semantic_maps = []
-    entropy_values = []
+    semantic_maps , entropy_values = [], []
     try:
         with torch.no_grad():
             for i, batch in enumerate(tqdm(test_loader)):
                 true_masks = batch["mask"].numpy()
                 for image, true_mask in zip(batch["image"], true_masks):
                     image = image.unsqueeze(0).to(device)  # add batch dimension because model expects it
+
+                    batch_mean = torch.mean(image, dim=[0, 2, 3])
+                    batch_std = torch.std(image, dim=[0, 2, 3])
+                    sum_mean += batch_mean
+                    sum_std += batch_std
+
                     pred = model(image)
 
                     softmax_probs = softmax(pred, dim=1)
-                    entropy = -torch.sum(softmax_probs * torch.log(softmax_probs + 1e-9),
-                                         dim=1)  # Sum over the classes, avoid log(0) by adding a small constant
+                    entropy = -torch.sum(softmax_probs * torch.log(softmax_probs + 1e-9), dim=1)
                     entropy = entropy.detach().cpu().numpy()
                     entropy_values.append(np.mean(entropy))
 
@@ -40,6 +47,12 @@ def predict(test_loader, df_test, level=0):
                     final_predictions.append(class_label.astype("uint8"))
                     semantic_maps.append(class_label.squeeze())
 
+            avg_mean = sum_mean / len(test_loader.dataset)
+            avg_std = sum_std / len(test_loader.dataset)
+
+            print(f"Avg Input Feature Mean in Test: {avg_mean.cpu().numpy()}")
+            print(f"Avg Input Feature Std in Test: {avg_std.cpu().numpy()}")
+
     except Exception as te:
         print(f"An exception occurred during inference: {te}")
     mean_iou = iou_metric.mean_iou()
@@ -48,4 +61,4 @@ def predict(test_loader, df_test, level=0):
     print(f"Overall average entropy for the entire test set: {overall_avg_entropy:.4f}")
     final_predictions = np.concatenate(final_predictions, axis=0)
     df_test = store_semantic_maps(df_test, level, semantic_maps)
-    return final_predictions, df_test, mean_iou
+    return final_predictions, df_test, mean_iou, overall_avg_entropy
