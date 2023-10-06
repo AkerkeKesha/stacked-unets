f3f9884183672211a5722f232045a15dbc156ceb
Akerke Balgabekova <akerke.kass@gmail.com>
2023-10-06 10:59:00 +0200

    factored out function that normalizes input given std/mean

--- src/utils.py
+++ src/utils.py
@@ -11,6 +11,10 @@ import config
 import matplotlib.pyplot as plt
 
 
+def normalize(feature, mean, std):
+    return (feature - mean) / std
+
+
 def grayscale_to_rgb(vv_image, vh_image):
     ratio_image = np.clip(np.nan_to_num(vh_image/(vv_image + 1e-6), 0), 0, 1)
     rgb_image = np.stack((vv_image, vh_image, 1-ratio_image), axis=2)
