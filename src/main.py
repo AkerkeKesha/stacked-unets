c05a6ab752f90b5c43dcbd81b781fb6b8f50aee5
Akerke Balgabekova <akerke.kass@gmail.com>
2023-10-09 21:47:46 +0200

    chores: refactored out helper functions that initialize default dict, compute statistics. Plot and report at the end

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import time
+from collections import defaultdict
 import numpy as np
 import pickle
 import matplotlib.pyplot as plt
@@ -63,16 +64,12 @@ def visualize_examples(df, n_samples=5, n_levels=1):
 
 
 def start_stacked_unet(n_levels, max_data_points, run_key, metrics):
-    for metric_name in metrics.keys():
-        if run_key not in metrics[metric_name]:
-            metrics[metric_name][run_key] = {}
-
     original_df, train_df, val_df, test_df, train_loader, val_loader, test_loader \
         = load_data(config.dataset, max_data_points=max_data_points)
 
     for level in range(n_levels):
         print(f"Level: [{level + 1} / {n_levels}]")
-        level_key = f"level_{level}"
+        level_key = f"level{level}"
 
         start = time.time()
         train_losses, val_losses, train_iou, val_iou, train_df, val_df \
@@ -81,74 +78,72 @@ def start_stacked_unet(n_levels, max_data_points, run_key, metrics):
         print(f"Takes {timing} seconds to train in level{level + 1}")
         final_predictions, test_df, mean_iou, avg_entropy = predict(test_loader, test_df, level=level)
 
-        for metric_name, metric_value in [('train_loss', train_losses),
-                                          ('val_loss', val_losses),
-                                          ('test_iou', mean_iou),
-                                          ('train_iou', train_iou),
-                                          ('val_iou', val_iou),
-                                          ('timing', timing),
-                                          ('entropy', avg_entropy),
-                                          ]:
-            if level_key not in metrics[metric_name][run_key]:
-                metrics[metric_name][run_key][level_key] = []
-            metrics[metric_name][run_key][level_key].append(metric_value)
+        metrics_matching = {
+            'train_loss': train_losses,
+            'val_loss': val_losses,
+            'test_iou': mean_iou,
+            'train_iou': train_iou,
+            'val_iou': val_iou,
+            'timing': timing,
+            'entropy': avg_entropy,
+        }
+        for metric_name, metric_value in metrics_matching.items():
+            update_metrics(metrics, metric_name, run_key, level_key, metric_value)
 
     np.save(f'{config.output_dir}/test_df_{run_key}.npy', test_df.to_dict(), allow_pickle=True)
     with open(f'{config.output_dir}/metrics.pkl', 'wb') as f:
         pickle.dump(metrics, f)
 
-    for level_key in metrics['train_loss'][run_key].keys():
-        for metric_name in ['train_loss', 'val_loss', 'train_iou', 'val_iou']:
-            plot_metric_with_error(metric_name, metrics, level_key)
 
-    for metric_name in ['test_iou', 'timing', 'entropy']:
-        plot_level_metrics(metrics, metric_name)
+def initialize_metrics(metric_names):
+    metrics = {}
+    for metric in metric_names:
+        metrics[metric] = defaultdict(lambda: defaultdict(list))
+    return metrics
 
-    # visualize_examples(test_df, n_samples=5, n_levels=n_levels)
+
+def update_metrics(metrics, metric_names, run_key, level, computed_metrics):
+    level_key = f"level{level}"
+    for metric_name in metric_names:
+        if metric_name in computed_metrics:
+            metrics[metric_name][run_key][level_key].append(computed_metrics[metric_name])
+
+
+def calculate_stat(metric_dict, metric_name, level_key):
+    if metric_name in metric_dict:
+        values = [metric_dict[metric_name][run_key][level_key] for run_key in metric_dict[metric_name]]
+        mean_value = np.mean(values)
+        std_value = np.std(values)
+        return mean_value, std_value
+    return None, None
 
 
 def run_experiments(runs=3, n_levels=1, max_data_points=None):
-    metrics = {
-        'train_loss': {},
-        'val_loss': {},
-        'train_iou': {},
-        'val_iou': {},
-        'test_iou': {},
-        'timing': {},
-        'entropy': {},
-    }
+    metrics = initialize_metrics(config.metrics)
     for run in range(runs):
         print(f"Run: [{run + 1} / {runs}]")
         run_key = f"run{run}"
         start_stacked_unet(n_levels, max_data_points, run_key, metrics)
 
-    with open(f'{config.output_dir}/metrics.pkl', 'rb') as f:
-        metrics = pickle.load(f)
-
     for level in range(n_levels):
-        level_key = f'level{level}'
-
-        mean_iou = np.mean([metrics['test_iou'][run_key][level_key] for run_key in metrics['test_iou']])
-        std_iou = np.std([metrics['test_iou'][run_key][level_key] for run_key in metrics['test_iou']])
-
-        mean_train_loss = np.mean([metrics['train_loss'][run_key][level_key] for run_key in metrics['train_loss']])
-        std_train_loss = np.std([metrics['train_loss'][run_key][level_key] for run_key in metrics['train_loss']])
-
-        mean_val_loss = np.mean([metrics['val_loss'][run_key][level_key] for run_key in metrics['val_loss']])
-        std_val_loss = np.std([metrics['val_loss'][run_key][level_key] for run_key in metrics['val_loss']])
-
-        mean_train_iou = np.mean([metrics['train_iou'][run_key][level_key] for run_key in metrics['train_iou']])
-        std_train_iou = np.std([metrics['train_iou'][run_key][level_key] for run_key in metrics['train_iou']])
-
-        mean_val_iou = np.mean([metrics['val_iou'][run_key][level_key] for run_key in metrics['val_iou']])
-        std_val_iou = np.std([metrics['val_iou'][run_key][level_key] for run_key in metrics['val_iou']])
-
         print(f"Level: {level + 1}")
-        print(f"Mean IoU: {mean_iou:.2f} +/- {std_iou:.2f}")
-        print(f"Mean Train Loss: {mean_train_loss:.2f} +/- {std_train_loss:.2f}")
-        print(f"Mean Validation Loss: {mean_val_loss:.2f}  +/- {std_val_loss:.2f}")
-        print(f"Mean Train IoU: {mean_train_iou:.2f} +/- {std_train_iou:.2f}")
-        print(f"Mean Validation IoU: {mean_val_iou:.2f}  +/- {std_val_iou:.2f}")
+        level_key = f'level{level}'
+        for metric_name in config.metrics:
+            mean_val, std_val = calculate_stat(metrics, metric_name, level_key)
+            if mean_val is not None and std_val is not None:
+                print(f"Mean {metric_name}: {mean_val:.2f} +/- {std_val:.2f}")
+
+    for metric_name in config.metrics:
+        for level in range(n_levels):
+            level_key = f"level{level}"
+            if metric_name in ['train_loss', 'val_loss', 'train_iou', 'val_iou']:
+                plot_metric_with_error(metric_name, metrics, level_key)
+
+    for metric_name in config.metrics:
+        if metric_name in ['test_iou', 'timing', 'entropy']:
+                plot_level_metrics(metrics, metric_name)
+
+    # visualize_examples(test_df, n_samples=5, n_levels=n_levels)
 
 
 
