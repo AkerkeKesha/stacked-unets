88929cd706c132885315ebdeec950823f7ee77f0
Akerke Balgabekova <akerke.kass@gmail.com>
2023-10-13 11:04:16 +0200

    chores: beautified docstring

--- src/analyze.py
+++ src/analyze.py
@@ -11,11 +11,11 @@ from src.utils import visualize_prediction
 def calculate_iou_score(mask_pred, mask_true, num_classes=2, smooth=0.0001):
     """
     Compute Intersection over Union (IoU) between true and predicted segmentation masks
-    :param mask_pred: Predicted mask
-    :param mask_true: Ground truth mask
-    :param num_classes: The number of classes for classification
-    :param smooth: Small value to avoid division by zero
-    :return: IoU
+    @param mask_pred: Predicted mask
+    @param mask_true: Ground truth mask
+    @param num_classes: The number of classes for classification
+    @param smooth: Small value to avoid division by zero
+    @return: IoU
     """
     conf_matrix = np.zeros((num_classes, num_classes))
     mask_pred = mask_pred.ravel()
@@ -41,10 +41,10 @@ def filter_positive_flood_masks(df, flood_label_column='flood_label_path'):
 def get_top_n_predictions(df, n, level=0):
     """
     Get the indices of top N best and worst predictions for specific level
-    :param df: DataFrame that stores IoU scores in columns named like 'iou_scores_level_x'
-    :param level: Level index to select IoU score column
-    :param n: Number of top samples to return
-    :return: Indices of top N best and worst predictions
+    @param df: DataFrame that stores IoU scores in columns named like 'iou_scores_level_x'
+    @param level: Level index to select IoU score column
+    @param n: Number of top samples to return
+    @return: Indices of top N best and worst predictions
     """
     iou_scores = df[f'iou_scores_level_{level}'].values
     best_n_idx = np.argsort(iou_scores)[-n:]
@@ -75,9 +75,9 @@ def visualize_best_worst_predictions(df, best_idx, worst_idx, n_levels=1,
 
 def compare_iou_scores(df, list_of_levels):
     """
-    :param df: DataFrame that contains IoU scores in columns named like 'iou_scores_level_x'
-    :param list_of_levels: List of levels to compare
-    :return: Dict containing improved, worsened, and constant indices for each level transition
+    @param df: DataFrame that contains IoU scores in columns named like 'iou_scores_level_x'
+    @param list_of_levels: List of levels to compare
+    @return: Dict containing improved, worsened, and constant indices for each level transition
     """
     results = {}
     for i in range(len(list_of_levels)):
@@ -140,10 +140,10 @@ def collect_metrics_from_files(metrics_base_names, source_dir, levels, dataset="
     """
     Collect metrics saved in npy files into a dictionary.
 
-    :param metrics_base_names: List of base names of metrics like ["train_losses", "val_losses", "train_iou", "val_iou"]
-    :param levels: List of levels like [0, 1, 2, 3]
-    :param dataset: Name of the dataset (default "etci")
-    :return: Dictionary containing the metrics
+    @param metrics_base_names: List of base names of metrics like ["train_losses", "val_losses", "train_iou", "val_iou"]
+    @param levels: List of levels like [0, 1, 2, 3]
+    @param dataset: Name of the dataset (default "etci")
+    @return: Dictionary containing the metrics
     """
     metrics_dict = {}
 
@@ -167,8 +167,8 @@ def plot_metrics(metrics_dict, metric_name='Loss'):
     """
     Plot metrics like loss or IoU for each level.
 
-    :param metrics_dict: Dictionary where keys are level identifiers and values are lists of metric values over epochs.
-    :param metric_name: Name of the metric ('Loss', 'IoU', etc.)
+    @param metrics_dict: Dictionary where keys are level identifiers and values are lists of metric values over epochs.
+    @param metric_name: Name of the metric ('Loss', 'IoU', etc.)
     """
     for level, level_metrics in metrics_dict.items():
         plt.plot(level_metrics, label=f"Level {level}")
